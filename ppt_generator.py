3080f3545d191d390e994f7ff9ebd9ee524584ec
Daniel <dfsanchezc@outlook.com>
2025-02-20 17:01:44 -0300

    Fixed file name mistake

--- ppt_generator.py
+++ ppt_generator.py
@@ -9,7 +9,7 @@ from pptx.dml.color import RGBColor
 
 
 def create_and_get_ppt_path(retail_name: str, type_name: str, amount_formatted: str, OC_specific_number: str) -> Path:
-    ppt_filename = f"{type_name.upper()} {type_name.upper()} (${amount_formatted.replace(',','.')}) - {OC_specific_number}.pptx"
+    ppt_filename = f"{type_name.upper()} {retail_name.upper()} (${amount_formatted.replace(',','.')}) - {OC_specific_number}.pptx"
     
     folder_path = Path.home() / "Desktop" / "PPTs" / f"{retail_name}" / f"{type_name}"
     folder_path.mkdir(parents=True, exist_ok=True)
